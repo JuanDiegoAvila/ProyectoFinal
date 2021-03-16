a8a63180e5d039e84161ebaeecf7be93b3f97038
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2021-03-16 11:55:21 -0600

    Main Menu

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public GameObject LevelLoader;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    public void fps()
+    {
+        
+        StartCoroutine(SceneLoad(1)); 
+    }
+
+    public void Cave()
+    {
+        StartCoroutine(SceneLoad(2)); 
+    }
+
+    public void exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    private IEnumerator SceneLoad(int scene)
+    {
+        LevelLoader.SetActive(!LevelLoader.activeSelf);
+        yield return SceneManager.LoadSceneAsync(scene);
+        LevelLoader.SetActive(!LevelLoader.activeSelf);
+    }
+}
