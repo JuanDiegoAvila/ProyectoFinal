75981e867bc0b033a0ddb3c9e70fe6a0c3e7c67c
Manuel-Archila <60369044+Manuel-Archila@users.noreply.github.com>
2021-03-16 15:35:29 -0600

    Trigger de los enemigos

---
+++ Assets/Scripts/CaveTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveTrigger : MonoBehaviour
+{
+    public GameObject spawnpoint;
+    public GameObject lich;
+    bool activated;
+    // Start is called before the first frame update
+    void Start()
+    {
+        activated = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(activated == false)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Instantiate(lich, spawnpoint.transform.position, Quaternion.identity);
+            }
+        }
+        activated = true;
+    }
+}
---
+++ Assets/Scripts/TriggerBoss.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBoss : MonoBehaviour
+{
+    public GameObject spawnpoint;
+    public GameObject boss;
+    bool activated;
+    // Start is called before the first frame update
+    void Start()
+    {
+        activated = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (activated == false)
+        {
+                Instantiate(boss, spawnpoint.transform.position, Quaternion.identity);
+        }
+        activated = true;
+    }
+}
