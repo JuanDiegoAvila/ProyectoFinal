09be7527a8a1b2853ec7e84842533de17ac640e5
Manuel-Archila <60369044+Manuel-Archila@users.noreply.github.com>
2021-03-17 16:32:37 -0600

    fixes

---
+++ Assets/Scripts/WinCave.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class WinCave : MonoBehaviour
+{
+    public GameObject Win;
+    bool activated;
+    public FirstPersonController fpsController;
+    Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Win.SetActive(false);
+        activated = false;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (activated == false)
+        {
+            Time.timeScale = 0.0f;
+            Win.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            FirstPersonController.active = false;
+            player.enabled = false;
+        }
+        activated = true;
+
+    }
+
+}
