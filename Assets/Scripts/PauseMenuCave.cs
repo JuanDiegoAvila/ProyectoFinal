b5d30255254e2a746471b3fb0531621147b6fb79
Manuel-Archila <60369044+Manuel-Archila@users.noreply.github.com>
2021-03-18 10:47:18 -0600

    Cave Scripts

---
+++ Assets/Scripts/PauseMenuCave.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuCave : MonoBehaviour
+{
+    public FirstPersonController fpsController;
+    PlayerCave player;
+
+    public GameObject pauseMenu;
+    public GameObject configMenu;
+    public GameObject LevelLoader;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        FirstPersonController.Enabled = true;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCave>();
+
+        pauseMenu.SetActive(false);
+
+        Time.timeScale = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            TogglePause();
+        }
+    }
+
+    public void ToggleConfig()
+    {
+        if (configMenu)
+        {
+            configMenu.SetActive(!configMenu.activeSelf);
+        }
+    }
+
+    public void TogglePause()
+    {
+        if (pauseMenu)
+        {
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+
+            FirstPersonController.active = !FirstPersonController.active;
+
+
+            if (pauseMenu.activeSelf)
+            {
+                player.enabled = false;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                player.enabled = true;
+            }
+
+            Time.timeScale = pauseMenu.activeSelf ? 0.0f : 1.0f;
+        }
+
+    }
+
+    public void restartGame()
+    {
+        Time.timeScale = 1.0f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        StartCoroutine(SceneLoad(SceneManager.GetActiveScene().buildIndex));
+    }
+
+    public void mainMenu()
+    {
+        Time.timeScale = 1.0f;
+
+        StartCoroutine(SceneLoad(0));
+    }
+
+    public void resume()
+    {
+        TogglePause();
+    }
+
+    private IEnumerator SceneLoad(int scene)
+    {
+        LevelLoader.SetActive(true);
+
+        yield return new WaitForSeconds(0.1f);
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(scene);
+
+        while (!operation.isDone)
+            yield return null;
+
+    }
+}
---
+++ Assets/Scripts/PlayerCave.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCave : MonoBehaviour
+{
+    public float health;
+
+    public Text healthText;
+    public Slider healthSlider;
+
+    public AudioSource shot;
+
+    public ParticleSystem bullet;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthSlider.value = health / 100;
+        healthText.text = health + "";
+
+        Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            shot.Play();
+            bullet.Play();
+
+            if (Physics.Raycast(myRay, out hitInfo))
+            {
+                if (hitInfo.collider.CompareTag("Enemy"))
+                {
+                    hitInfo.collider.GetComponentInParent<Enemy>().health -= 20f;
+                    hitInfo.collider.GetComponentInParent<Enemy>().GetComponent<Animator>().SetBool("GetHit", true);
+                }
+            }
+
+        }
+
+
+    }
+
+}
